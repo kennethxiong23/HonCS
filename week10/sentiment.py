521d909b50774d027c4de30456b4d170262ef4f1
Kenneth <kenneth.xiong.23@bishops.com>
2021-08-30 20:34:12 -0700

    Finish sentiment.oy

--- week10/sentiment.py
+++ week10/sentiment.py
@@ -6,21 +6,12 @@
     Name: Kenneth
     Date: Fall 2021
 """
-# def readLine(filename):
-#     """open given filename, read in text, split into words, return list of words"""
-#     readFile = open(filename)
-#     words = []
-#     for line in readFile:
-#         line = line.strip()
-#         words.append(line)
 
-#     return words
-from os import remove
-
-def readFile(fileName):
+def readReviews (fileName):
     readFile = open(fileName)
     ratingAndWords = []
     for line in readFile:
+        line = line.lower()
         line = line.strip()
         line = line.split()
         rating = convertScore(int(line[0]))
@@ -30,7 +21,7 @@ def readFile(fileName):
 
 def convertScore(rating):
     """convert the score from 1-5 to -2-2"""
-    score = rating - 3
+    score = rating - 2
     return score
 
 def findWord(word,  knownWords):
@@ -40,16 +31,50 @@ def findWord(word,  knownWords):
             return knownWords.index(score)
     return -1
 
-def removeWords(commonWords, allWords):
+def removeWords(stopWords, allReviews):
     "remove common words - where r ur proper block commentssssssssss"
-    commonWords = open("stopwords")
-    commonWords = commonWords.strip()
-    print(commonWords)
+    readFile = open(stopWords)
+    commonWords = []
+    for word in readFile:
+        word = word.strip()
+        commonWords.append(word)
+    for review in allReviews:
+        keepWords = []
+        for word in review[1]:
+            if word not in commonWords:
+                keepWords.append(word)
+            review[1] = keepWords
+
+def bubbleSort(listOfLists):
+    """ This is the bubble sort algorithm:
+            - given a list L
+            - for every item in the list, compare to the item just to the right, swap if needed
+            - keep doing the above until you go from one end of the list to the
+              other and don't make any swaps!
+    """
+    for i in range(len(listOfLists)):
+        for j in range(len(listOfLists) - 1):
+            first = listOfLists[j][0]
+            second  = listOfLists[j + 1][0]
+            if first  > second:
+                listOfLists[j][0], listOfLists[j + 1][0] = listOfLists[j + 1][0], listOfLists [j][0]
+                listOfLists[j][1], listOfLists[j + 1][1] = listOfLists[j + 1][1], listOfLists [j][1]
+    listOfLists.reverse()
+    print(listOfLists)
+
+def printReviews(reviews):
+    print("top 20:")
+    for review in reviews[:20]:
+        print("%s: %s" %(review[0], review[1]))
+    print("bottom 20:")
+    for review in reviews[-20:]:
+        print("%s: %s" %(review[0], review[1]))
 
 def main():
-    removeWords(se)
-    fileName = "smallReviews.txt"
-    allReviews = readFile(fileName)
+    fileName = "movieReviews.txt"
+    commonsWords = "stopwords.txt"
+    allReviews = readReviews(fileName)
+    removeWords(commonsWords, allReviews)
     sentimentScore = []
     for review in allReviews:
         rating = review[0]
@@ -60,7 +85,9 @@ def main():
             else:
                 score = sentimentScore.pop(index)
                 sentimentScore.append([score[0] + rating, word])
-    print(sentimentScore)
+    bubbleSort(sentimentScore)
+    printReviews(sentimentScore)
+
 
 
         
