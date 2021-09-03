8649c685878d69ac0159c2bdcc526fde69aa7a5f
Kenneth <kenneth.xiong.23@bishops.com>
2021-09-02 21:16:08 -0700

    Sutff was dohne, i haven't had a signle actually commit message in decades

--- week10/visual-sort.py
+++ week10/visual-sort.py
@@ -5,18 +5,45 @@
 """
 from random import shuffle
 
-def main():
-    L = [1,2,3,4,5,6,7]
-    L = shuffle(L)
-    print(L)
-    for i in range(len(L)):
-        for j in range(len(L) - 1):
-            first = L[j]
-            second  = L[j + 1]
-            if first  > second:
-                L[j], L[j + 1] = L[j + 1], L [j]
+def generateList(length):
+    """make a random list with specified length"""
+    L = []
+    for i in range(length):
+        L.append(i)
+    shuffle(L)
+    return L
 
-    print(L)
+def printList(list, index, SWAP):
+    """Shows the visual aspect of if it's switched or not"""
+    stringList = []
+    for i in range(len(list) ):
+        if i == index or i == index + 1:
+            stringList.append("|%s|" %list[i] )
+        else:
+            stringList.append(list[i])
+    if SWAP:
+        print
+    print(*stringList)
+
+
+def main():
+    length = int(input("How long would you like the list to be: "))
+    list = generateList(length)
+
+    for i in range(len(list)):
+        print("Pass #%s" %(i + 1))
+        for j in range(len(list) - 1):
+            first = list[j]
+            second  = list[j + 1]
+            if first  > second:
+                printList(list, j, True)
+                list[j], list[j + 1] = list[j + 1], list[j]
+            else:
+                printList(list, j, False)
+
+        
+    print(list)
+
 
 
 
